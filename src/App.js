21a3e5893b31203a153fce7740899d8b5c88d784
iamoki <76725512+iamoki@users.noreply.github.com>
2022-02-15 23:09:22 +0900

    동적 context 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import ColorBox from './components/ColorBox';
-import ColorContext from './contexts/color';
+import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
 
 const App = () => {
   return (
-    // Provider로 value 덮어쓰기
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
-    </ColorContext.Provider>
-  )
-}
+    </ColorProvider>
+  );
+};
 
 export default App;
